d7afeb12b98ffc48aaef44c70296d3246fa84c9f
Shawn Catanzarite <scatanzarite@gmail.com>
2012-08-03 01:22:28 -0700

    make sure cucumber cleans up directories when it's done

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,9 +3,16 @@ $:.unshift File.expand_path("lib/")
 require 'fileutils'
 require 'volley'
 
+root = Dir.pwd
+
 Volley::Log.add(:debug, "#{Dir.pwd}/log/volley.log")
 Volley::Log.console_disable
 
 World do
   @dir = Dir.pwd
+end
+
+at_exit do
+  puts "cleaning up... #{root}"
+  %w{local remote}.each { |d| FileUtils.rm_rf("#{root}/test/publisher/#{d}") }
 end
