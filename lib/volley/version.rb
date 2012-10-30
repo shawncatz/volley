9d18eeb9b5c66260a3aa2081b2396af269919225
Shawn Catanzarite <me@shawncatz.com>
2012-10-30 15:13:52 -0700

    v0.1.15

--- lib/volley/version.rb
+++ lib/volley/version.rb
@@ -3,7 +3,7 @@ unless defined?(Volley::Version)
     module Version
       MAJOR  = 0
       MINOR  = 1
-      TINY   = 14
+      TINY   = 15
       TAG    = nil
       STRING = [MAJOR, MINOR, TINY, TAG].compact.join('.')
     end
